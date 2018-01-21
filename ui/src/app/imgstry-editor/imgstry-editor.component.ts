e712686c3fbebe7a69bf85d3eece76073ba49a06
Vlad Jerca <vlad.jerca@gmail.com>
2018-01-21 20:37:28 +0200

    ui: update data model for ng component

--- ui/src/app/imgstry-editor/imgstry-editor.component.ts
+++ ui/src/app/imgstry-editor/imgstry-editor.component.ts
@@ -1,29 +1,66 @@
-import { Component, OnInit, OnChanges, SimpleChanges, AfterViewInit, ElementRef, ViewChild, Renderer, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  Renderer,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+
+import { FilterOption } from '../../../../source/core/types/index';
 import { Imgstry } from '../../../../source/core/imgstry.browser';
 
+export interface ImgstryValues {
+  brightness?: number;
+  contrast?: number;
+}
 @Component({
   selector: 'app-imgstry-editor',
   templateUrl: './imgstry-editor.component.html',
-  styleUrls: ['./imgstry-editor.component.scss']
+  styleUrls: ['./imgstry-editor.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImgstryEditorComponent implements OnInit, OnChanges, AfterViewInit {
   @ViewChild('canvas')
   canvas: ElementRef;
-  processor: Imgstry;
-  @Input('brightness-value') bValue: number;
+  @Input()
+  brightness: number;
+  @Input()
+  contrast: number;
 
-  constructor(private renderer: Renderer) { }
+  public processor: Imgstry;
 
-  ngOnInit() {
-  }
+  constructor(
+    private renderer: Renderer,
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (!this.processor)
-      return;
-    if (changes['bValue']) {
-      this.processor.reset();
-      this.processor.brightness(this.bValue);
+    if (!this.processor) { return; }
+
+    const options: FilterOption[] = [];
+    if (changes['brightness']) {
+      options.push({
+        filter: 'brightness',
+        value: this.brightness,
+        priority: 1
+      });
     }
+    if (changes['contrast']) {
+      options.push({
+        filter: 'contrast',
+        value: this.contrast,
+        priority: 1
+      });
+    }
+    this.processor.batch(options, true);
   }
 
   ngAfterViewInit() {
