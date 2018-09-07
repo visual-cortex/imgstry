479c6db5b6e66239fb5596d962a111f676505282
Vlad Jerca <vlad.jerca@gmail.com>
2018-09-07 20:12:11 +0300

    core: extract operation logic

--- source/core/imgstry.processor.ts
+++ source/core/imgstry.processor.ts
@@ -8,6 +8,7 @@ import {
 } from '../pixel';
 
 import { Kernel } from '../kernel';
+import { Operation } from './imgstry.operation';
 
 /** TO-DO:
  *    - integral blur (+ other blur methods)
@@ -28,15 +29,6 @@ interface TraversalPixelInfo {
 }
 
 export abstract class ImgstryProcessor {
-  /**
-   * The processing caching dictionary.
-   *
-   * @private
-   * @static
-   * @type {Record<string, number[]>}
-   * @memberof ImgstryProcessor
-   */
-  private static LookupCache: Record<string, number[]> = {};
   /**
    * Width of the image
    *
@@ -121,183 +113,48 @@ export abstract class ImgstryProcessor {
     return histogramResult;
   }
 
-  public bw(ratio?: Array<number>): ImgstryProcessor {
-    if (!(!!ratio && Array.isArray(ratio) && ratio.reduce((a: any, b: any) => { return a + b; }, 0) <= 1)) {
-      ratio = [0.3, 0.59, 0.11];
-    }
-
-    return this._traverse((pixel: Rgb) => {
-      let bwValue = ratio[0] * pixel.r + ratio[1] * pixel.g + ratio[2] * pixel.b;
-
-      pixel.r = bwValue;
-      pixel.g = bwValue;
-      pixel.b = bwValue;
-
-      return pixel;
-    });
+  public blackAndWhite(ratio?: [number, number, number]): ImgstryProcessor {
+    return this._traverse(Operation.blackAndWhite(ratio));
   }
 
   public contrast(value: number): ImgstryProcessor {
-    if (value < 0) {
-      value /= 10;
-    }
-
-    value = Math.pow((value + 100) / 100, 2);
-
-    const lookup = ImgstryProcessor.LookupCache['contrast'] =
-      ImgstryProcessor.LookupCache['contrast'] ||
-      this._lookup((i) => {
-        i /= 255;
-        i -= 0.5;
-        i *= value;
-        i += 0.5;
-        i *= 255;
-
-        return i;
-      });
-
-    return this._traverse((pixel: Rgb) => {
-      pixel.r = lookup[pixel.r];
-      pixel.g = lookup[pixel.g];
-      pixel.b = lookup[pixel.b];
-
-      return pixel;
-    });
+    return this._traverse(Operation.contrast(value));
   }
 
   public brightness(value: number): ImgstryProcessor {
-    value = Math.floor(255 * (value / 100));
-
-    return this._traverse((pixel: Rgb) => {
-      pixel.r += value;
-      pixel.g += value;
-      pixel.b += value;
-
-      return pixel;
-    });
+    return this._traverse(Operation.brightness(value));
   }
 
   public saturation(value: number): ImgstryProcessor {
-    value *= -0.01;
-
-    const lookup = ImgstryProcessor.LookupCache['saturation'] =
-      ImgstryProcessor.LookupCache['saturation'] ||
-      this._lookup((i) => {
-        return i * value;
-      });
-
-    return this._traverse((pixel: Rgb) => {
-      let max = Math.max(pixel.r, pixel.g, pixel.b);
-
-      pixel.r += lookup[max - pixel.r];
-      pixel.g += lookup[max - pixel.g];
-      pixel.b += lookup[max - pixel.b];
-
-      return pixel;
-    });
+    return this._traverse(Operation.saturation(value));
   }
 
   public hue(value: number): ImgstryProcessor {
-    value *= 0.5;
-
-    return this._traverse((pixel: Rgb) => {
-      let hsv = pixel.toHsv();
-
-      hsv.h += Math.abs(value);
-
-      return hsv.toRgb();
-    });
+    return this._traverse(Operation.hue(value));
   }
 
   public sepia(value: number): ImgstryProcessor {
-    if (!value) {
-      value = 100;
-    }
-    value /= 100;
-
-    return this._traverse((pixel: Rgb) => {
-      pixel.r = (pixel.r * (1 - (0.607 * value))) + (pixel.g * (0.769 * value)) + (pixel.b * (0.189 * value));
-      pixel.g = (pixel.r * (0.349 * value)) + (pixel.g * (1 - (0.314 * value))) + (pixel.b * (0.168 * value));
-      pixel.b = (pixel.r * (0.272 * value)) + (pixel.g * (0.534 * value)) + (pixel.b * (1 - (0.869 * value)));
-
-      return pixel;
-    });
+    return this._traverse(Operation.sepia(value));
   }
 
   public gamma(value: number): ImgstryProcessor {
-    if (value >= 0) {
-      value = 1 - (value / 100);
-    } else {
-      value /= -10;
-    }
-
-    const lookup = ImgstryProcessor.LookupCache['gamma'] =
-      ImgstryProcessor.LookupCache['gamma'] ||
-      this._lookup((i) => {
-        return Math.pow(i / 255, value) * 255;
-      });
-
-    return this._traverse((pixel: Rgb) => {
-      pixel.r = lookup[pixel.r];
-      pixel.g = lookup[pixel.g];
-      pixel.b = lookup[pixel.b];
-
-      return pixel;
-    });
+    return this._traverse(Operation.gamma(value));
   }
 
   public noise(value: number): ImgstryProcessor {
-    return this._traverse((pixel: Rgb) => {
-      let random = Math.random() * value * 2.55;
-      random = (Math.random() > 0.5 ? -random : random);
-
-      pixel.r += random;
-      pixel.b += random;
-      pixel.g += random;
-
-      return pixel;
-    });
+    return this._traverse(Operation.noise(value));
   }
 
   public vibrance(value: number): ImgstryProcessor {
-    value *= -1;
-
-    return this._traverse((pixel: Rgb) => {
-      let amount: number;
-      let average: number;
-      let max: number;
-
-      max = Math.max(pixel.r, pixel.g, pixel.b);
-      average = (pixel.r + pixel.g + pixel.b) / 3;
-      amount = ((Math.abs(max - average) * 2 / 255) * value) / 100;
-
-      pixel.r += (max - pixel.r) * amount;
-      pixel.g += (max - pixel.g) * amount;
-      pixel.b += (max - pixel.b) * amount;
-
-      return pixel;
-    });
+    return this._traverse(Operation.vibrance(value));
   }
 
   public invert(): ImgstryProcessor {
-    return this._traverse((pixel: Rgb) => {
-      pixel.r ^= 255;
-      pixel.g ^= 255;
-      pixel.b ^= 255;
-
-      return pixel;
-    });
+    return this._traverse(Operation.invert());
   }
 
   public tint(color: string): ImgstryProcessor {
-    let tint = new Hex(color).toRgb();
-    return this._traverse((pixel: Rgb) => {
-      pixel.r = pixel.r + (255 - pixel.r) * (tint.r / 255);
-      pixel.g = pixel.g + (255 - pixel.g) * (tint.g / 255);
-      pixel.b = pixel.b + (255 - pixel.b) * (tint.b / 255);
-
-      return pixel;
-    });
+    return this._traverse((Operation.tint(color)));
   }
 
   public convolve(kernel: Kernel | number[][], factor = 1): ImgstryProcessor {
@@ -361,14 +218,6 @@ export abstract class ImgstryProcessor {
     for (let option of options) {
       (<any>this)[option.filter](option.value);
     }
-  }
-
-  private _lookup(delegate: (i: number) => number): number[] {
-    const arr: any[] = [];
-    for (let i = 0; i < 256; i++) {
-      arr[i] = delegate(i);
-    }
-    return arr;
   }
 
   private _traverse = (delegate: (pixel: Rgb, information?: TraversalPixelInfo) => Rgb | void): ImgstryProcessor => {
