92586821740062f1a694623ff7f39022c64f34fb
Vlad Jerca <vlad.jerca@gmail.com>
2018-09-07 20:30:26 +0300

    tests: define functional contexts

--- test/end-to-end/imgstry.test.ts
+++ test/end-to-end/imgstry.test.ts
@@ -27,253 +27,270 @@ describe('imgstry', () => {
     takeScreenshot(this.currentTest);
   });
 
-  it('should get canvas', () => {
-    board.should.be.not.empty;
-  });
+  context('ctor', () => {
+    it('should be able to fetch the canvas reference from the DOM', () => {
+      board.should.be.not.empty;
+    });
 
-  it('should construct correctly', () => {
-    processor.width.should.equal(500);
-    processor.height.should.equal(500);
-  });
+    it('should correctly populate the width and height properties', () => {
+      processor.width.should.equal(500);
+      processor.height.should.equal(500);
+    });
 
-  it('should be black', () => {
-    let pixelData = processor.imageData.data;
+    it('should have all pixels black when constructed', () => {
+      let pixelData = processor.imageData.data;
 
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
 
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-
-    channelSum.should.equal(0);
-  });
-
-  it('should invert to white', () => {
-    processor.invert();
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-    (channelSum / pixelData.length * 4).should.equal(255);
-  });
-
-  it('should be neutral gray', () => {
-    processor.brightness(50);
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-    (channelSum / pixelData.length * 4).should.oneOf([127, 128]);
-  });
-
-  it('should apply sepia to #c0392b', () => {
-    processor.brightness(50)
-      .tint('#c0392b')
-      .sepia(100);
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    let rgb = { r: 0, g: 0, b: 0 };
-
-    for (let i = 0; i < pixelData.length; i += 4) {
-      rgb.r += pixelData[i];
-      rgb.g += pixelData[i + 1];
-      rgb.b += pixelData[i + 2];
-    }
-
-    (rgb.r / pixelData.length * 4).should.equal(236);
-    (rgb.g / pixelData.length * 4).should.equal(214);
-    (rgb.b / pixelData.length * 4).should.equal(198);
-  });
-
-  it('should should tint pomegranate', () => {
-    processor.brightness(50);
-    processor.tint('#c0392b');
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-    (channelSum / pixelData.length * 4).should.equal(176);
-  });
-
-  it('should should tint green sea', () => {
-    processor.brightness(50);
-    processor.tint('#16a085');
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-    (channelSum / pixelData.length * 4).should.approximately(180, 1);
-  });
-
-  it('should turn bw', () => {
-    processor.brightness(50);
-    processor.tint('#16a085');
-    processor.blackAndWhite();
-    let pixelData = processor.imageData.data;
-
-    let channelSum = true;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum = rgb.r === rgb.b && rgb.b === rgb.g;
-      if (channelSum) {
-        break;
-      }
-    }
-    channelSum.should.be.equal(true);
-  });
-
-  it('should contrast color', () => {
-    processor.brightness(50);
-    processor.tint('#16a085');
-    processor.contrast(20);
-    let pixelData = processor.imageData.data;
-
-    let channelSum = 0;
-    for (let i = 0; i < pixelData.length; i += 4) {
-      let rgb = {
-        r: pixelData[i],
-        g: pixelData[i + 1],
-        b: pixelData[i + 2],
-      };
-
-      channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-    }
-    (channelSum / pixelData.length * 4).should.approximately(202, 1);
-  });
-
-  it('should have sum of channel color distribution equal to 1', () => {
-    processor.brightness(15).noise(25).tint('#16a085');
-    const result = processor.histogram;
-    result.all.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
-    result.channels.red.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
-    result.channels.green.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
-    result.channels.blue.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
-  });
-
-  it('should apply edge detection kernel succesfully', (done) => {
-    const image = new Image();
-    image.onload = () => {
-      try {
-        processor.context.drawImage(image, 0, 0);
-
-        let pixelData = processor.imageData.data;
-        let initialAlpha = 0;
-        for (let i = 0; i < pixelData.length; i += 4) {
-          initialAlpha += pixelData[i + 3];
-        }
-
-        processor.convolve(imgstry.Utility.EdgeDetection);
-
-        pixelData = processor.imageData.data;
-        let channelSum = 0;
-        let alpha = 0;
-
-        for (let i = 0; i < pixelData.length; i += 4) {
-          let rgb = {
-            r: pixelData[i],
-            g: pixelData[i + 1],
-            b: pixelData[i + 2],
-            a: pixelData[i + 3],
-          };
-
-          alpha += rgb.a;
-          channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-        }
-        (channelSum / pixelData.length).should
-          .approximately(
-            0, 1.51,
-            'edge detection should generate 98.5% darkness',
-          );
-        alpha.should.equal(initialAlpha, 'the alpha channel was mutated by the convolution');
-      } catch (e) {
-        return done(e);
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
       }
 
-      done();
-    };
-    image.src = 'rnm.jpg';
+      channelSum.should.equal(0);
+    });
   });
 
-  it('should apply the gaussion blur kernel succesfully', (done) => {
-    const image = new Image();
-    image.onload = () => {
-      try {
-        processor.context.drawImage(image, 0, 0);
+  context('invert', () => {
+    it('should have all pixels white if invert is applied', () => {
+      processor.invert();
+      let pixelData = processor.imageData.data;
 
-        let pixelData = processor.imageData.data;
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
 
-        processor.convolve(imgstry.Utility.GaussianBlur(5, 50));
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+      }
+      (channelSum / pixelData.length * 4).should.equal(255);
+    });
+  });
 
-        let channelSum = 0;
+  context('brightness', () => {
+    it('should have all pixels neutral gray if brigthness is set to 50', () => {
+      processor.brightness(50);
+      let pixelData = processor.imageData.data;
 
-        processor.convolve(imgstry.Utility.EdgeDetection);
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
 
-        pixelData = processor.imageData.data;
-        for (let i = 0; i < pixelData.length; i += 4) {
-          let rgb = {
-            r: pixelData[i],
-            g: pixelData[i + 1],
-            b: pixelData[i + 2],
-            a: pixelData[i + 3],
-          };
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+      }
+      (channelSum / pixelData.length * 4).should.oneOf([127, 128]);
+    });
+  });
 
-          channelSum += (rgb.r + rgb.b + rgb.g) / 3;
-        }
-        (channelSum / pixelData.length).should
-          .approximately(
-            0,
-            .51,
-            'edge detection after blur should generate at least 99.5% darkness',
-          );
-      } catch (e) {
-        return done(e);
+  context('sepia', () => {
+    it('should apply sepia to #c0392b', () => {
+      processor.brightness(50)
+        .tint('#c0392b')
+        .sepia(100);
+      let pixelData = processor.imageData.data;
+
+      let channelSum = 0;
+      let rgb = { r: 0, g: 0, b: 0 };
+
+      for (let i = 0; i < pixelData.length; i += 4) {
+        rgb.r += pixelData[i];
+        rgb.g += pixelData[i + 1];
+        rgb.b += pixelData[i + 2];
       }
 
-      done();
-    };
-    image.src = 'rnm.jpg';
+      (rgb.r / pixelData.length * 4).should.equal(236);
+      (rgb.g / pixelData.length * 4).should.equal(214);
+      (rgb.b / pixelData.length * 4).should.equal(198);
+    });
+  });
+
+  context('tint', () => {
+    it('should apply pomegranate to neutral grey', () => {
+      processor.brightness(50);
+      processor.tint('#c0392b');
+      let pixelData = processor.imageData.data;
+
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
+
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+      }
+      (channelSum / pixelData.length * 4).should.equal(176);
+    });
+
+    it('should apply green sea to neutral grey', () => {
+      processor.brightness(50);
+      processor.tint('#16a085');
+      let pixelData = processor.imageData.data;
+
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
+
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+      }
+      (channelSum / pixelData.length * 4).should.approximately(180, 1);
+    });
+  });
+
+  context('black and white', () => {
+    it('should have the same value on all channels', () => {
+      processor.brightness(50);
+      processor.tint('#16a085');
+      processor.blackAndWhite();
+      let pixelData = processor.imageData.data;
+
+      let channelSum = true;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
+
+        channelSum = channelSum &&
+          rgb.r ===
+          rgb.b &&
+          rgb.b ===
+          rgb.g;
+      }
+      channelSum.should.be.equal(true);
+    });
+  });
+
+  context('contrast', () => {
+    it('should increase color intensity', () => {
+      processor.brightness(50);
+      processor.tint('#16a085');
+      processor.contrast(20);
+      let pixelData = processor.imageData.data;
+
+      let channelSum = 0;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        let rgb = {
+          r: pixelData[i],
+          g: pixelData[i + 1],
+          b: pixelData[i + 2],
+        };
+
+        channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+      }
+      (channelSum / pixelData.length * 4).should.approximately(202, 1);
+    });
+  });
+
+  context('histogram', () => {
+    it('should have sum of channel color distribution equal to 1', () => {
+      processor.brightness(15).noise(25).tint('#16a085');
+      const result = processor.histogram;
+      result.all.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
+      result.channels.red.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
+      result.channels.green.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
+      result.channels.blue.reduce((a: number, b: number) => a + b, 0).should.approximately(1, .00000001);
+    });
+  });
+
+  context('convolution', () => {
+
+    context('edge detection', () => {
+      it('should have 98.% percent of pixels black after detection', (done) => {
+        const image = new Image();
+        image.onload = () => {
+          try {
+            processor.context.drawImage(image, 0, 0);
+
+            let pixelData = processor.imageData.data;
+            let initialAlpha = 0;
+            for (let i = 0; i < pixelData.length; i += 4) {
+              initialAlpha += pixelData[i + 3];
+            }
+
+            processor.convolve(imgstry.Utility.EdgeDetection);
+
+            pixelData = processor.imageData.data;
+            let channelSum = 0;
+            let alpha = 0;
+
+            for (let i = 0; i < pixelData.length; i += 4) {
+              let rgb = {
+                r: pixelData[i],
+                g: pixelData[i + 1],
+                b: pixelData[i + 2],
+                a: pixelData[i + 3],
+              };
+
+              alpha += rgb.a;
+              channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+            }
+            (channelSum / pixelData.length).should
+              .approximately(0, 1.51);
+            alpha.should.equal(initialAlpha, 'the alpha channel was mutated by the convolution');
+          } catch (e) {
+            return done(e);
+          }
+
+          done();
+        };
+        image.src = 'rnm.jpg';
+      });
+    });
+
+    context('gaussian blur', () => {
+      it('should have 99.8% of pixels black after applying a 9x9 kernel and edge detection', (done) => {
+        const image = new Image();
+        image.onload = () => {
+          try {
+            processor.context.drawImage(image, 0, 0);
+
+            let pixelData = processor.imageData.data;
+
+            processor.convolve(imgstry.Utility.GaussianBlur(9, 100));
+
+            let channelSum = 0;
+
+            processor.convolve(imgstry.Utility.EdgeDetection);
+
+            pixelData = processor.imageData.data;
+            for (let i = 0; i < pixelData.length; i += 4) {
+              let rgb = {
+                r: pixelData[i],
+                g: pixelData[i + 1],
+                b: pixelData[i + 2],
+                a: pixelData[i + 3],
+              };
+
+              channelSum += (rgb.r + rgb.b + rgb.g) / 3;
+            }
+            (channelSum / pixelData.length).should
+              .approximately(0, .2);
+          } catch (e) {
+            return done(e);
+          }
+
+          done();
+        };
+        image.src = 'rnm.jpg';
+      });
+    });
   });
 });
