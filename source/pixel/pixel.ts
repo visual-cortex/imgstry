8ab7d77b46cc6302f5990fd20c294f7c44a07590
Vlad Jerca <vlad.jerca@gmail.com>
2018-09-10 00:39:44 +0300

    core: fix pixel generic template definition

--- source/pixel/pixel.ts
+++ source/pixel/pixel.ts
@@ -1,9 +1,21 @@
-export class Pixel<IColor> {
+import { IColor } from './color';
+
+/**
+ *
+ *
+ * @export
+ * @class Pixel
+ * @template IColor
+ */
+export class Pixel<T extends IColor> {
   public x: number;
   public y: number;
-  public color: IColor;
 
-  constructor(x: number, y: number) {
+  constructor(
+    x: number,
+    y: number,
+    public color?: T,
+  ) {
     this.x = x || 0;
     this.y = y || 0;
   }
