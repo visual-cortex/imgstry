41bb1329bfcf06c0f195c3c59ff12d73080ce921
Vlad Jerca <vlad.jerca@gmail.com>
2018-12-07 02:09:20 +0200

    test: implement hue operation scenarios

--- test/unit/core/imgstry.operation.test.ts
+++ test/unit/core/imgstry.operation.test.ts
@@ -1,3 +1,4 @@
+import { Hsv } from '../../../source/pixel/color/spaces/hsv';
 import { Operation } from '../../../source/core/imgstry.operation';
 import { Rgb } from '../../../source/pixel/color/spaces/rgb';
 import { expect } from 'chai';
@@ -58,5 +59,64 @@ describe('Image operations', () => {
         expect(bRatio).approximately(ratio[2], 1e-9);
       }
     });
+  });
+
+  context('hue', () => {
+    [0, 30, 60, 120, 180]
+      .forEach(hue => {
+        it(`should shift hue value by ${hue}`, () => {
+          const pixel: Hsv = new Hsv({
+            h: 240,
+            s: .6,
+            v: .6,
+          });
+
+          const result: Hsv = Operation.hue(hue)(pixel.toRgb()).toHsv();
+          const expected = pixel.h + hue;
+          if (
+            expected === 0 ||
+            expected === 360
+          ) {
+            expect(result.h).oneOf([0, 360]);
+          } else {
+            expect(result.h).equal((pixel.h + hue) % 361);
+          }
+        });
+      });
+
+    [-180, -120, -60, -30, 0]
+      .forEach(hue => {
+        it(`should shift hue value by ${hue}`, () => {
+          const pixel: Hsv = new Hsv({
+            h: 20,
+            s: .6,
+            v: .6,
+          });
+
+          const result: Hsv = Operation.hue(hue)(pixel.toRgb()).toHsv();
+          if (
+            hue === 0 ||
+            hue === 360
+          ) {
+            expect(result.h).equal(pixel.h);
+          } else {
+            expect(result.h).equal((360 + pixel.h + hue) % 361);
+          }
+        });
+      });
+
+    [100, 200, 300, 400, 500]
+      .forEach(hue => {
+        it('should NOT overflow with high value shift', () => {
+          const pixel: Hsv = new Hsv({
+            h: 200,
+            s: .6,
+            v: .6,
+          });
+
+          const result: Hsv = Operation.hue(hue)(pixel.toRgb()).toHsv();
+          expect(result.h).equal((pixel.h + hue) % 361);
+        });
+      });
   });
 });
