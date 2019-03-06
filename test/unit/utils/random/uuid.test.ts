4cc9ff960b6ebea7510314abb6625af3c46e2e61
Vlad Jerca <vlad.jerca@gmail.com>
2019-03-06 02:46:09 +0200

    test: write tests for the uuid util

---
+++ test/unit/utils/random/uuid.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { uuid } from '~utils/random';
+
+describe('randomUtil: uuid', () => {
+  it('should be a valid UUID according to RFC4122', () =>
+    Array(1000).fill(void 0).map(() => uuid()).forEach(id =>
+      expect(id).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i),
+    ),
+  );
+});
